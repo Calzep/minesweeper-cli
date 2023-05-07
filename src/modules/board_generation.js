a5577d20c76a0e81a3c87da3f39ab7aef2c885ac
Calzep <caleb-easpn@nmit.live.ac.nz>
2023-05-08 11:33:56 +1200

    Created version 1 of board gen module

--- src/modules/board_generation.js
+++ src/modules/board_generation.js
@@ -3,3 +3,21 @@ version 0.0, 04/05/2023
 Caleb Eason
 
 Module to generate and display a 9 by 9 minesweeper game board*/
+
+//Variables and Constants
+const BOARD_DIMENSION = 3 //Specifies the amount of rows and columns in the board, hardcoded
+
+function initialiseBoard(dimension){
+    let gameBoard = []
+    for (let i = 0; i < dimension; i++){
+        gameBoard.push([])
+        for (let j = 0; j <dimension; j++){
+            gameBoard[i].push(j+1+i*dimension)
+        }
+    }
+    return gameBoard
+    
+}
+
+const gameBoard = initialiseBoard(BOARD_DIMENSION)
+console.log(gameBoard)
