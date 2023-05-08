8ff8b14247097f22a78e812b20da7f0b7b2e408b
Calzep <caleb-easpn@nmit.live.ac.nz>
2023-05-08 15:47:34 +1200

    refactored mine_placemet to use map instead of for

--- src/modules/mine_placement.js
+++ src/modules/mine_placement.js
@@ -27,32 +27,35 @@ var gameBoard = [   //global variable, taken form board gen module
 function placeMines(min,max,dimension){
     //Randomly calculate how any mines to place on the board
     let mineCount = Math.floor(Math.random() * (max - min + 1)) + min
-    console.log('mineCount:',mineCount) //*for debugging
+    //console.log('mineCount:',mineCount) //*for debugging
 
     //place mines on the board, iterates through the loop for each mines that needs to be placed
     for (let i = 0; i < mineCount; i++){
         //Loop to prevent placing multiple mines in the same cell
-        outerLoop: while (true) {
+        while (true) {
             //generate a random x and y coordinate within range of the game board
             let x_candidate = Math.floor(Math.random() * (dimension)) + 1
             let y_candidate = Math.floor(Math.random() * (dimension)) + 1
             //console.log('x and y candidate:',x_candidate,y_candidate) //*for debugging
             
-            //Iterate through the game board until a cell is found with matching x and y values to x_candidate and y_candidate
-            for (let i = 0; i < gameBoard.length; i++){
-                if (gameBoard[i].x == x_candidate && gameBoard[i].y == y_candidate) {
-                    //console.log('matched cell',gameBoard[i],'\n')   //*for debugging
+            /*find the index of an object whose x and y values match those of x_candidate and y_candidate.
 
-                    //When a matching cell is found, check if it already contains a mine
-                    //If there is not already a mine in the cell, a new mine will be placed and the while loop will break,
-                    //otherwise the mine will not be placed and new cooridanates will be generated
-                    if (gameBoard[i].containsMine == false){
-                        gameBoard[i].containsMine = true
-                        //console.log('loop broken, cell value:',gameBoard[i],'\n') //*for debugging
-                        break outerLoop
-                    }
-                    
+            maps the property values for each object in the list into an array, then iterates over the list, checking
+            if the values of x and y match x_candidate and y_candidate.  When a match is found, the index of the 
+            matching object is returned, otherwise undefined is returned.  The filter funciton removes the undefined values*/
+            arrIndex = gameBoard.map((element, index) => {
+                if (element.x == x_candidate && element.y == y_candidate){
+                    return index
                 }
+            }).filter(element => element >=0)
+            //console.log('matched cell',gameBoard[arrIndex],'\n')   //*for debugging
+
+            //Checks if the matched cell contains a mine.  If it doesn't, a mine is placed and the loop is broken
+            //if there is already a mine present, the loop repeats and a new set of coordinates are generated
+            if (gameBoard[arrIndex].containsMine == false){
+                gameBoard[arrIndex].containsMine = true
+                //console.log('loop broken, cell value:',gameBoard[arrIndex],'\n') //*for debugging
+                break
             }
         }
     }
