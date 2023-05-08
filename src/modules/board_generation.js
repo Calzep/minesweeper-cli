b19f72e1725b1302ef2ffcc798cd6d54c1be4976
Calzep <caleb-easpn@nmit.live.ac.nz>
2023-05-08 16:34:44 +1200

    switch x and y back, it was better the first way

--- src/modules/board_generation.js
+++ src/modules/board_generation.js
@@ -9,13 +9,13 @@ const BOARD_DIMENSION = 3 //Specifies the amount of rows and columns in the boar
 var gameBoard = []  //Global Variable for holding cell information
 
 function initialiseBoard(dimension){
-    //iterate through each row
+    //iterate through each column
     for (let i = 0; i < dimension; i++){
-        //iterate thorugh each column for row i
+        //iterate thorugh each row for column i
         for (let j = 0; j < dimension; j++){
             //Creates an object for each cell in the grid
             //x and y refer to the graphical position of each cell, state can be 'covered', 'uncovered' or flagged'
-            gameBoard.push({x:j+1,y:i+1,containsMine:false,state:'covered',minesNearby:0})
+            gameBoard.push({x:i+1,y:j+1,containsMine:false,state:'covered',minesNearby:0})
         }
     }
     return gameBoard
--- src/modules/mine_counter.js
+++ src/modules/mine_counter.js
@@ -2,4 +2,21 @@
 version 0.0, 04/05/2023
 Caleb Eason
 
-Module to count the nearby mines and display the result as a number on the game board*/
+Module to count how many mines neighbour a cell and save the value to the cell object*/
+
+var gameBoard = [ //global variable, taken form board gen module
+    { x: 1, y: 1, containsMine: false, state: 'covered', minesNearby: 0 },
+    { x: 1, y: 2, containsMine: true, state: 'covered', minesNearby: 0 },
+    { x: 1, y: 3, containsMine: false, state: 'covered', minesNearby: 0 },
+    { x: 2, y: 1, containsMine: true, state: 'covered', minesNearby: 0 },
+    { x: 2, y: 2, containsMine: true, state: 'covered', minesNearby: 0 },
+    { x: 2, y: 3, containsMine: true, state: 'covered', minesNearby: 0 },
+    { x: 3, y: 1, containsMine: false, state: 'covered', minesNearby: 0 },
+    { x: 3, y: 2, containsMine: false, state: 'covered', minesNearby: 0 },
+    { x: 3, y: 3, containsMine: false, state: 'covered', minesNearby: 0 }
+]
+function countNearbyMines () {
+    for (let i = 0; i < gameBoard.length; i++){
+        
+    }
+}
