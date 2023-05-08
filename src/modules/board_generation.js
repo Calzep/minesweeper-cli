76935576d812d971366e86061ccc2a31287b391f
Calzep <caleb-easpn@nmit.live.ac.nz>
2023-05-08 12:21:41 +1200

    Version 2 of board gen module

--- src/modules/board_generation.js
+++ src/modules/board_generation.js
@@ -5,20 +5,20 @@ Caleb Eason
 Module to generate and display a 9 by 9 minesweeper game board*/
 
 //Variables and Constants
-const BOARD_DIMENSION = 3 //Specifies the amount of rows and columns in the board, hardcoded
+const BOARD_DIMENSION = 2 //Specifies the amount of rows and columns in the board, hardcoded
 
 function initialiseBoard(dimension){
     let gameBoard = []
-    //for each row in the grid add a new nested list
+    //iterate through each row
     for (let i = 0; i < dimension; i++){
-        gameBoard.push([])
-        //for each column in row i, add an integer
-        for (let j = 0; j <dimension; j++){
-            gameBoard[i].push(j+1+i*dimension)
+        //iterate thorugh each column for row i
+        for (let j = 0; j < dimension; j++){
+            //Creates an object for each cell in the grid
+            //x and y refer to the graphical position of each cell, state can be 'covered', 'uncovered' or flagged'
+            gameBoard.push({x:i+1,y:j+1,containsMine:false,state:'covered',minesNearby:0})
         }
     }
     return gameBoard
-    
 }
 
 const gameBoard = initialiseBoard(BOARD_DIMENSION)
