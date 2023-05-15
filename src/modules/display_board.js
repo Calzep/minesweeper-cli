7a46fb787245951c77e8317fb017f20c96a86fa9
Calzep <caleb-easpn@nmit.live.ac.nz>
2023-05-16 09:29:01 +1200

    Added logic to display axis keys

--- src/modules/display_board.js
+++ src/modules/display_board.js
@@ -1,89 +1,224 @@
 /* SDV503 Minesweeper game, display board module
-version 2, 15/05/2023
+version 3, 16/05/2023
 Caleb Eason
 
 Module to generate a visual display of the game board*/
-const BOARD_DIMENSION = 3 //Specifies the amount of rows and columns in the board, hardcoded
+const BOARD_DIMENSION = 5 //Specifies the amount of rows and columns in the board, hardcoded
 var gameBoard = [   //global variable, taken form board gen module
 {
-    x: 1,
-    y: 1,
-    containsMine: false,
-    state: 'flagged',
-    minesNearby: 0,
-    display: '?'
-  },
-  {
-    x: 1,
-    y: 2,
-    containsMine: false,
-    state: 'uncovered',
-    minesNearby: 0,
-    display: '░'
-  },
-  {
-    x: 1,
-    y: 3,
-    containsMine: false,
-    state: 'uncovered',
-    minesNearby: 1,
-    display: '1'
-  },
-  {
-    x: 2,
-    y: 1,
-    containsMine: false,
-    state: 'covered',
-    minesNearby: 3,
-    display: ' '
-  },
-  {
-    x: 2,
-    y: 2,
-    containsMine: false,
-    state: 'covered',
-    minesNearby: 3,
-    display: ' '
-  },
-  {
-    x: 2,
-    y: 3,
-    containsMine: false,
-    state: 'covered',
-    minesNearby: 2,
-    display: ' '
-  },
-  {
-    x: 3,
-    y: 1,
-    containsMine: true,
-    state: 'flagged',
-    minesNearby: 2,
-    display: 'X'
-  },
-  {
-    x: 3,
-    y: 2,
-    containsMine: true,
-    state: 'covered',
-    minesNearby: 2,
-    display: ' '
-  },
-  {
-    x: 3,
-    y: 3,
-    containsMine: false,
-    state: 'uncovered',
-    minesNearby: 1,
-    display: '1'
-  }
+  x: 1,
+  y: 1,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: ' '
+},
+{
+  x: 1,
+  y: 2,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: ' '
+},
+{
+  x: 1,
+  y: 3,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: ' '
+},
+{
+  x: 1,
+  y: 4,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: ' '
+},
+{
+  x: 1,
+  y: 5,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: ' '
+},
+{
+  x: 2,
+  y: 1,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: ' '
+},
+{
+  x: 2,
+  y: 2,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: ' '
+},
+{
+  x: 2,
+  y: 3,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: ' '
+},
+{
+  x: 2,
+  y: 4,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: ' '
+},
+{
+  x: 2,
+  y: 5,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: ' '
+},
+{
+  x: 3,
+  y: 1,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: ' '
+},
+{
+  x: 3,
+  y: 2,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: ' '
+},
+{
+  x: 3,
+  y: 3,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: ' '
+},
+{
+  x: 3,
+  y: 4,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: ' '
+},
+{
+  x: 3,
+  y: 5,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: ' '
+},
+{
+  x: 4,
+  y: 1,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: ' '
+},
+{
+  x: 4,
+  y: 2,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: ' '
+},
+{
+  x: 4,
+  y: 3,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: '?'
+},
+{
+  x: 4,
+  y: 4,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 3,
+  display: '3'
+},
+{
+  x: 4,
+  y: 5,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 2,
+  display: '2'
+},
+{
+  x: 5,
+  y: 1,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: ' '
+},
+{
+  x: 5,
+  y: 2,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: 'X'
+},
+{
+  x: 5,
+  y: 3,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 2,
+  display: '2'
+},
+{
+  x: 5,
+  y: 4,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: '░'
+},
+{
+  x: 5,
+  y: 5,
+  containsMine: false,
+  state: 'covered',
+  minesNearby: 0,
+  display: '░'
+}
 ]
 
 function displayBoard(){
     console.clear() //clear space in the console
 
+    //Create X axis key
+    let xkey = '     1'
+    for (let i = 2; i <= BOARD_DIMENSION; i++){
+      xkey += `   ${i}`
+    }
+    console.log(xkey)
+
     //Create the top border of the board, use a for loop to create a variable amount of cells
-    let startRow = '╔═══'
+    let startRow = '   ╔═══'
     //console.log(startRow)   //*for debugging
     for (let i = 1; i < BOARD_DIMENSION; i++){
         startRow += '╦═══'
@@ -96,7 +231,7 @@ function displayBoard(){
     for (let i = 1; i < BOARD_DIMENSION; i++){
 
         //Create the left and right cell boarders
-        let middleRow1 = `║ ${gameBoard[(i-1)*BOARD_DIMENSION].display} `
+        let middleRow1 = ` ${i} ║ ${gameBoard[(i-1)*BOARD_DIMENSION].display} `
         for (let j = 1; j < BOARD_DIMENSION; j++){
             middleRow1 += `║ ${gameBoard[j+(i-1)*BOARD_DIMENSION].display} `
         }
@@ -104,7 +239,7 @@ function displayBoard(){
         console.log(middleRow1)
 
         //create the top boarder of the next row        
-        let middleRow2 = '╠═══'
+        let middleRow2 = '   ╠═══'
         for (let j = 1; j < BOARD_DIMENSION; j++){
             middleRow2 += '╬═══'
         }
@@ -114,7 +249,7 @@ function displayBoard(){
     }
 
     //Create the left and right cell boarders of the final row
-    let endRow1 = `║ ${gameBoard[(BOARD_DIMENSION**2-BOARD_DIMENSION)].display} `
+    let endRow1 = ` ${BOARD_DIMENSION} ║ ${gameBoard[(BOARD_DIMENSION**2-BOARD_DIMENSION)].display} `
     for (let i = 1; i < BOARD_DIMENSION; i++){
         endRow1 += `║ ${gameBoard[(BOARD_DIMENSION**2-BOARD_DIMENSION)+i].display} `
     }
@@ -122,7 +257,7 @@ function displayBoard(){
     console.log(endRow1)
 
     //Create bottom boarder of the final row
-    let endRow2 = '╚═══'
+    let endRow2 = '   ╚═══'
     for (let i = 1; i < BOARD_DIMENSION; i++){
         endRow2 += '╩═══'
     }
