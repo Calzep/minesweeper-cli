b9da89ceb2ad4e3f8444d1f1977c7048066d382a
Calzep <caleb-easpn@nmit.live.ac.nz>
2023-05-15 12:50:08 +1200

    finished win conditions module

--- src/modules/win_condition.js
+++ src/modules/win_condition.js
@@ -3,3 +3,31 @@ version 0.0, 04/05/2023
 Caleb Eason
 
 Module to check if the user has uncovered every cell and end the game if true*/
+
+var gameBoard = [   //global variable, taken form board gen module
+    { x: 1, y: 1, containsMine: true, state: 'covered', minesNearby: 0 },
+    { x: 1, y: 2, containsMine: false, state: 'uncovered', minesNearby: 0 },
+    { x: 1, y: 3, containsMine: false, state: 'uncovered', minesNearby: 0 },
+    { x: 2, y: 1, containsMine: false, state: 'uncovered', minesNearby: 0 },
+    { x: 2, y: 2, containsMine: false, state: 'uncovered', minesNearby: 0 },
+    { x: 2, y: 3, containsMine: false, state: 'uncovered', minesNearby: 0 },
+    { x: 3, y: 1, containsMine: false, state: 'uncovered', minesNearby: 0 },
+    { x: 3, y: 2, containsMine: true, state: 'covered', minesNearby: 0 },
+    { x: 3, y: 3, containsMine: false, state: 'uncovered', minesNearby: 0 }
+  ]
+
+function checkWinCondition(){
+    let gamefinished = true
+    for (let i = 0; i < gameBoard.length; i++){
+        if(!(gameBoard[i].state === 'uncovered' || gameBoard[i].containsMine === true)){
+            gamefinished = false
+            console.log('win conditions not met')   //*for debugging
+            break
+        }
+    }
+    if (gamefinished === true){
+        console.log('game won, ending game')    //*for debugging
+    }
+}
+
+checkWinCondition()
