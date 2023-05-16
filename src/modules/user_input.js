c4786ef4319c1593c9feb0766a26d70a8a986f0e
Calzep <caleb-easpn@nmit.live.ac.nz>
2023-05-16 12:43:56 +1200

    Created user input module

--- src/modules/user_input.js
+++ src/modules/user_input.js
@@ -3,3 +3,26 @@ version 0.0, 15/05/2023
 Caleb Eason
 
 Module to receive and interpret user input*/
+
+const readline = require('node:readline/promises');
+const {stdin: input, stdout: output} = require('node:process');
+
+
+
+const askQuestion = async (question) => {
+    const rl = readline.createInterface({input,output});
+    let userInput = await rl.question(question)
+    rl.close()
+
+    if(true){
+        console.log(userInput)
+    } else {
+        console.log('error')
+        askQuestion(question)
+    }
+    return userInput
+}
+
+(async () => {
+    console.log(askQuestion("Enter an X value\n"))
+})();
